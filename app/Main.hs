4f4976c5042e4b9583b7b2d02f1d8a65c094f2fa
xayon40-12 <touroux.nathan@gmail.com>
2020-11-24 21:14:13 +0100

    add gloss graphics

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,56 @@
 module Main where
 
+import Data.Array.Unboxed
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
 import KT
 
-every = 10
+type State = (Vec T, Vec T)
 
-v = 0.3
-z = repeat 0
-o = repeat 1
-n = 50
-a = take n $ take 10 o ++ z :: Vec T
-l=1
-dx = l/ fromIntegral n
-dt = dx/v/10
+n = div (width -10) 2
+
+list = zip [0 ..] $ take n $ take (div n 3) (repeat 1) ++ (repeat 0)
+
+a = array (0, n -1) list
+
+b = array (0, n -1) list
+
+v = 1
+
+dx = 1 / fromIntegral width
+
+dt = dx / v / 10
+
+skip = 10
+
+fd = c
 
 f :: T -> T
-f u = v*u
+f u = v * u
 
 f' :: T -> T
 f' _ = v
 
+width = 1200 :: Int
+
+hight = 600 :: Int
+
 main :: IO ()
-main = do
-    mapM_ disp $ [iterate (next dx dt c f f') a !! i | i <- [0,every..]]
+main = play (InWindow "KT" (width, hight) (0, 0)) white 100 (a, b) makePicture handleEvent stepWorld
+
+makePicture :: State -> Picture
+makePicture (u, v) =
+  pictures $
+    draw u (- fromIntegral width / 2) ++ draw v 0 ++ [polygon [(0, - h2), (0, h2), (-10, h2), (-10, - h2)]]
+  where
+    h2 = h / 2
+    h = fromIntegral hight
+    draw u start = [color blue $ line [(x, - h2), (x, h - h2)] | (x, h) <- zip [start ..] $ map ((* h) . realToFrac) $ elems u]
+
+handleEvent :: Event -> State -> State
+handleEvent _ = id
+
+stepWorld :: Float -> State -> State
+stepWorld _ (u, v) = (it c u, it c2 v)
+  where
+    it fd = (!! skip) . iterate (next dx dt fd f f')
--- src/KT.hs
+++ src/KT.hs
@@ -1,80 +1,74 @@
-{-# language BangPatterns #-}
+{-# LANGUAGE BangPatterns #-}
+
 module KT where
-    
-import System.IO
+
 import Control.Concurrent
---import Data.Array.ST hiding (index)
---import Data.Array.Unboxed hiding (index)
---import Control.Monad
---import Control.Monad.ST
+import Control.Monad
+import Data.Array.ST hiding (index)
+import Data.Array.Unboxed hiding (index)
+import System.IO
 
 type T = Double
-type Vec a = [a]
+
+type Vec a = UArray Int a
+
 type Id = Int
 
-disp :: Vec T -> IO ()
-disp xs = do
-    putStr $! "\ESC[2J\ESC[H" ++ disp' xs
-    hFlush stdout
-    threadDelay 100000
-    where disp' (x:xs) = take (floor (x*mul)) (cycle "#") ++ "\n" ++ tail
-              where !tail = disp' $ drop 0 xs
-          disp' _ = ""
-          mul = 100
-
 minmod :: [T] -> T
-minmod [a,b] = (signum a + signum b)/2*min (abs a) (abs b)
-minmod (a:b:xs) = minmod $ minmod [a,b] : xs
-
-index :: Vec T -> Id -> T
-index u i = u !! mod i l
-    where l = length u
-
-type Func = T -> T
-type Evolve = T -> Func -> Func -> Vec T -> Vec T
-
-c :: Evolve
-c dx f f' u = c' <$> [0..length u -1]
-    where c' i = -(hp - hm)/dx
-            where upp = up - dx/2*uxp
-                  ump = uc + dx/2*ux
-                  upm = uc - dx/2*ux
-                  umm = um + dx/2*uxm
-                  um2 = index u (i-2)
-                  um = index u (i-1)
-                  uc = index u i
-                  up = index u (i+1)
-                  up2 = index u (i+2)
-                  uxm = minmod [theta*(um-um2),(uc-um2)/2,theta*(uc-um)] /dx
-                  ux = minmod [theta*(uc-um),(up-um)/2,theta*(up-uc)] /dx
-                  uxp = minmod [theta*(up-uc),(up2-uc)/2,theta*(up2-up)] /dx
-                  ap = max (rhof upp) (rhof ump)
-                  am = max (rhof upm) (rhof umm)
-                  hp = (f upp + f ump - ap*(upp - ump))/2
-                  hm = (f upm + f umm - am*(upm - umm))/2
-                  rhof = rho f'
-                  theta = 1.7 -- WARNING modifiable parameter
-
-
-c2 :: Evolve
-c2 dx f fp u = c2' <$> [0..l-1]
-    where l = length u
-          c2' i = -ux/dx
-            where um = f $ index u (i-1)
-                  uc = f $ index u i
-                  up = f $ index u (i+1)
-                  ux = if fp (index u i) < 0
-                       then up-uc
-                       else uc-um
-
-next :: T -> T -> Evolve -> Func -> Func -> Vec T -> Vec T
-next dx dt e f f' u = u3
-    where u1 = zipWith (\x y -> x+dt*y) u (c u)
-          u2 = zipWith3 (\u u1 cu1 -> n1*u + (1-n1)*(u1+dt*cu1)) u u1 (c u1)
-          u3 = zipWith3 (\u u2 cu2 -> n2*u + (1-n2)*(u2+dt*cu2)) u u2 (c u2)
-          n1 = 3/4
-          n2 = 1/3
-          c = e dx f f'
+minmod [a, b] = (signum a + signum b) / 2 * min (abs a) (abs b)
+minmod (a : b : xs) = minmod $ minmod [a, b] : xs
 
 rho :: Func -> T -> T
 rho f' u = abs $ f' u
+
+type Func = T -> T
+
+type Evolve = T -> Func -> Func -> Vec T -> Id -> T
+
+c :: Evolve
+c dx f f' u i = - (hp - hm) / dx
+  where
+    s = (\(a, b) -> b - a) . bounds $ u
+    theta = 1.7
+    l = s + 1
+    um2 = u ! mod (i -2) l
+    um = u ! mod (i -1) l
+    uc = u ! mod i l
+    up = u ! mod (i + 1) l
+    up2 = u ! mod (i + 2) l
+    uxm = minmod [theta * (um - um2), (uc - um2) / 2, theta * (uc - um)] / dx
+    ux = minmod [theta * (uc - um), (up - um) / 2, theta * (up - uc)] / dx
+    uxp = minmod [theta * (up - uc), (up2 - uc) / 2, theta * (up2 - up)] / dx
+    upp = up - dx / 2 * uxp
+    ump = uc + dx / 2 * ux
+    upm = uc - dx / 2 * ux
+    umm = um + dx / 2 * uxm
+    rhof = rho f'
+    ap = max (rhof upp) (rhof ump)
+    am = max (rhof upm) (rhof umm)
+    hp = (f upp + f ump - ap * (upp - ump)) / 2
+    hm = (f upm + f umm - am * (upm - umm)) / 2
+
+c2 :: Evolve
+c2 dx f fp u i = - ux / dx
+  where
+    s = (\(a, b) -> b - a) . bounds $ u
+    l = s + 1
+    um = f $ u ! mod (i -1) l
+    uc = f $ u ! mod i l
+    up = f $ u ! mod (i + 1) l
+    ux =
+      if fp (u ! mod i l) < 0
+        then up - uc
+        else uc - um
+
+next :: T -> T -> Evolve -> Func -> Func -> Vec T -> Vec T
+next dx dt e f f' u = runSTUArray $ do
+  let s = (\(a, b) -> b - a) . bounds $ u
+  let c = e dx f f'
+  tu <- newArray (0, s) 0
+  forM_ [0 .. s] $ \i -> do
+    let x = u ! i
+    let y = c u i
+    writeArray tu i $ x + dt * y
+  return tu
