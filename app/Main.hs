67b4ce7d1ed24b3a108fe4237537fd863101f560
xayon40-12 <touroux.nathan@gmail.com>
2020-11-25 17:40:02 +0100

    smaller window, add burger equation

--- app/Main.hs
+++ app/Main.hs
@@ -9,56 +9,58 @@ import KT (kt)
 
 data Sym = Sym Evolve System
 
-type State = (Sym, Sym)
+type State = [Sym]
 
 n = div (width -10) 2
 
 h2 = fromIntegral $ div (hight -10) 2
 w2 = fromIntegral $ div (width -10) 2
 
-list = zip [0 ..] $ take n $ replicate (div n 3) h2 ++ repeat 0
+gate frac = zip [0 ..] $ take n $ replicate (div n frac) h2 ++ repeat 0
+sinus f = [(i,h2/2*(1+sin (fromIntegral i*2*pi*f/fromIntegral n))) | i <- [0..n-1]]
 
-a = Sym kt $ Sys f f' n (array (0, n -1) list)
-
-b = Sym bf $ Sys f f' n (array (0, n -1) list)
+sys f f' init val = Sys f f' n (array (0, n -1) (init val))
+a = [Sym kt (sys cont cont' gate 3)
+    ,Sym bf (sys cont cont' gate 3)
+    ,Sym kt (sys burger burger' gate 2)
+    ,Sym bf (sys burger burger' sinus 1)]
 
 v = 1
 
 dx = 1 / w2
 
-dt = dx / v / 10
+dt = dx / v / 5
 
-skip = 10
 
-f :: T -> T
-f u = v * u
+bur = 0.003
+burger u = bur*u*u
+burger' u = 2*bur*u
+cont u = v*u
+cont' _ = v
 
-f' :: T -> T
-f' _ = v
+width = 400 :: Int
 
-width = 1200 :: Int
-
-hight = 1200 :: Int
+hight = 400 :: Int
 
 main :: IO ()
-main = play (InWindow "KT" (width, hight) (0, 0)) white 100 (a, b) makePicture handleEvent stepWorld
+main = play (InWindow "KT" (width, hight) (0, 0)) white 200 a makePicture handleEvent stepWorld
 
 makePicture :: State -> Picture
-makePicture (Sym _ (Sys _ _ _ u), Sym _ (Sys _ _ _ v)) =
+makePicture s | [u,v,w,x] <- [u | (Sym _ (Sys _ _ _ u)) <- s] =
   pictures $
-       draw u (-w2)
-    ++ draw v 6
+       draw u (-w2) 5
+    ++ draw v 6 5
+    ++ draw w (-w2) (-h2)
+    ++ draw x 6 (-h2)
     ++ [polygon [(5, - h2), (5, h2), (-6, h2), (-6, - h2)]
-       ,polygon [(-w2,-5),(w2,-5),(w2,5),(-w2,5)]]
+       ,polygon [(-w2-1,-5),(w2,-5),(w2,5),(-w2-1,5)]]
   where
     h2 = fromIntegral hight /2
     w2 = fromIntegral width /2
-    draw u start = [color blue $ line [(x, 5), (x, 5+h)] | (x, h) <- zip [start ..] $ map realToFrac $ elems u]
+    draw u sx sy = [color blue $ line [(x, sy), (x, sy+h)] | (x, h) <- zip [sx ..] $ map realToFrac $ elems u]
 
 handleEvent :: Event -> State -> State
 handleEvent _ = id
 
 stepWorld :: Float -> State -> State
-stepWorld _ (Sym cu u, Sym cv v) = (Sym cu $ it cu u, Sym cv $ it cv v)
-  where
-    it fd = (!! skip) . iterate (euler dx dt fd)
+stepWorld _ s = [Sym cu $ euler dx dt cu u | (Sym cu u) <- s]
