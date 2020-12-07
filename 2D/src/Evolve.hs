b1e5781fe2d0d988c6dcf43807473ed06eb04e5d
xayon40-12 <touroux.nathan@gmail.com>
2020-12-07 18:17:17 +0100

    use green when <0 and red when >0

--- 2D/src/Evolve.hs
+++ 2D/src/Evolve.hs
@@ -2,31 +2,33 @@
 
 module Evolve where
 
-import Data.Array.Accelerate (Acc, Array, DIM2, Exp, Word32, Boundary, Z (..), function, constant, generate, unlift, use, (:.) (..), (?))
+import Data.Array.Accelerate (Acc, Array, Boundary, DIM2, Exp, Word32, Z (..), constant, function, generate, unlift, use, (:.) (..), (?))
 import qualified Data.Array.Accelerate as A
 import qualified Data.Array.Accelerate.LLVM.Native as CPU
 import qualified Data.Array.Accelerate.LLVM.PTX as GPU
 import Graphics.Gloss.Accelerate.Data.Picture
-import Graphics.Gloss.Interface.Pure.Game
 import Graphics.Gloss.Data.ViewPort (ViewPort)
+import Graphics.Gloss.Interface.Pure.Game
 
 type T = Float
 
 type Arr = Array DIM2 T
 
 type Arr2 = Array DIM2 (T, T)
+
 type Func = Exp T -> Exp T
 
-data Conf = Conf {_f :: Func
-                 ,_f' :: Func
-                 ,_g :: Func
-                 ,_g' :: Func
-                 ,_dx :: Exp T
-                 ,_dt :: Exp T
-                 ,_ev :: Evolve
-                 }
+data Conf = Conf
+  { _f :: Func,
+    _f' :: Func,
+    _g :: Func,
+    _g' :: Func,
+    _dx :: Exp T,
+    _dt :: Exp T,
+    _ev :: Evolve
+  }
 
-type System = (Conf,Arr)
+type System = (Conf, Arr)
 
 type Evolve = Conf -> Acc Arr -> Acc Arr
 
@@ -55,31 +57,31 @@ arr = res
 
 makePicture :: State -> Picture
 makePicture s =
-  pictures $!
-    pics
-      ++ [ polygon [(5, - h2 -1), (5, h2), (-6, h2), (-6, - h2 -1)],
-           polygon [(- w2 -1, -6), (w2, -6), (w2, 5), (- w2 -1, 5)]
-         ]
+  pictures
+    $! pics
+    ++ [ polygon [(5, - h2 -1), (5, h2), (-6, h2), (-6, - h2 -1)],
+         polygon [(- w2 -1, -6), (w2, -6), (w2, 5), (- w2 -1, 5)]
+       ]
   where
     h2 = fromIntegral hight + 5
     w2 = fromIntegral width + 5
     wp = w2 / 2 + 3
-    hp = h2 / 2 + 2
+    hp = h2 / 2 + 3
     pics =
       [ translate x y $
           bitmapOfArray (CPU.run1 (A.map toWord32) a) True
-        | ((x, y), (_,a)) <- zip [(- wp, hp), (wp, hp), (- wp, - hp-2), (wp, - hp-2)] s
+        | ((x, y), (_, a)) <- zip [(- wp, hp), (wp, hp), (- wp, - hp -2), (wp, - hp -2)] s
       ]
 
 toWord32 :: Exp T -> Exp Word32
-toWord32 f = rgba v v 1 1
+toWord32 f = (f A.> 1) A.? (rgba 1 0 0 1, (f A.< 0) ? (rgba 0 1 0 1, rgba v v 1 1))
   where
     v = 1 - f
 
 rgba :: Exp T -> Exp T -> Exp T -> Exp T -> Exp Word32
 rgba i j k l = r + 256 * (g + 256 * (b + 256 * a))
   where
-    [r, g, b, a] = [{-max 0 $ min 255 $ -}A.floor (c * 255) | c <- [i, j, k, l]]
+    [r, g, b, a] = [A.floor (c * 255) | c <- [i, j, k, l]]
 
 handleEvent :: Event -> State -> State
 handleEvent _ = id
@@ -88,9 +90,10 @@ stepWorld :: ViewPort -> Float -> State -> State
 stepWorld _ _ s = next <$> s
 
 next :: System -> System
-next (conf@(Conf _ _ _ _ _ dt c),u) = (conf,un)
+next (conf@(Conf _ _ _ _ _ dt c), u) = (conf, un)
   where
     un = GPU.run1 (\u -> u2 u $ u1 u) u
+    --pos = A.map (\a -> (a A.< 0) ? (0,a))
     c' = c conf
     u1 u = A.map (\v -> let (a, b) = unlift v in a + dt * b) $ A.zip u (c' u)
     n1 = 0.5
@@ -99,8 +102,8 @@ next (conf@(Conf _ _ _ _ _ dt c),u) = (conf,un)
         ( \v ->
             let (a, b, c) = unlift v
              in n1 * a + (1 - n1) * (b + dt * c)
-        ) $ A.zip3 u u1 (c' u1)
-
+        )
+        $ A.zip3 u u1 (c' u1)
 
 boundary :: Acc Arr -> Boundary Arr
 boundary _ = function $ const 0
--- 2D/src/KT.hs
+++ 2D/src/KT.hs
@@ -1,25 +1,31 @@
 module KT where
 
 import Data.Array.Accelerate
-import Prelude ()
 import Evolve
+import Prelude ()
 
 kt :: Evolve
 kt (Conf f f' g g' dx _ _) u = stencil skt (boundary u) u
-     where skt :: Stencil5x5 T -> Exp T
-           skt ((_ ,_,t2,_,_ )
-               ,(_ ,_,t ,_,_ )
-               ,(l2,l,c ,r,r2)
-               ,(_ ,_,b ,_,_ )
-               ,(_ ,_,b2,_,_ )) = -(  h f f' (l,c,r,r2) - h f f' (l2,l,c,r)
-                                    + h g g' (b,c,t,t2) - h g g' (b2,b,c,t)) / dx
+  where
+    skt :: Stencil5x5 T -> Exp T
+    skt
+      ( (_, _, t2, _, _),
+        (_, _, t, _, _),
+        (l2, l, c, r, r2),
+        (_, _, b, _, _),
+        (_, _, b2, _, _)
+        ) =
+        - ( h f f' (l, c, r, r2) - h f f' (l2, l, c, r)
+              + h g g' (b, c, t, t2) - h g g' (b2, b, c, t)
+          )
+          / dx
 
 h :: Func -> Func -> (Exp T, Exp T, Exp T, Exp T) -> Exp T
-h f f' (a,b,c,d) = (f up + f um - v * (up - um)) / 2
-    where
-        up = c - (ux_ (b,c,d) / 2)
-        um = b + (ux_ (a,b,c) / 2)
-        v = a_ f' up um
+h f f' (a, b, c, d) = (f up + f um - v * (up - um)) / 2
+  where
+    up = c - (ux_ (b, c, d) / 2)
+    um = b + (ux_ (a, b, c) / 2)
+    v = a_ f' up um
 
 a_ :: Func -> Exp T -> Exp T -> Exp T
 a_ f' up um = max (rhof up) (rhof um)
@@ -27,9 +33,9 @@ a_ f' up um = max (rhof up) (rhof um)
     rhof = abs . f'
 
 ux_ :: (Exp T, Exp T, Exp T) -> Exp T
-ux_ (um,uc,up) = minmod [theta * (uc - um), (up - um) / 2, theta * (up - uc)]
-    where
-        theta = 2
+ux_ (um, uc, up) = minmod [theta * (uc - um), (up - um) / 2, theta * (up - uc)]
+  where
+    theta = 1.1
 
 minmod :: [Exp T] -> Exp T
 minmod [a, b] = (signum a + signum b) / 2 * min (abs a) (abs b)
